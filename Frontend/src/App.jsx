a7568054902550f1f2570789fccda2af1f5ada0b
Aryan Yadav <aryan31424@gmail.com>
2025-05-09 12:50:53 +0530

    more components like login,register and footer added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,20 @@
 import './App.css'
 import VideoCard from './Components/VideoCard'
 import Home from './Pages/Home'
-
+import Login from './Pages/login'
+import Register from './Pages/Register'
+import Footer from './Components/Footer'
 function App() {
 
   return (
     <>
       <div>
         {/* <h1>TeleHeath Web App</h1> */}
-        <Home/>
-        <VideoCard/>
+        {/* <Home/> */}
+        {/* <Register/> */}
+        <Footer/>
+        {/* <Login/> */}
+        {/* <VideoCard/> */}
       </div>
     </>
   )
