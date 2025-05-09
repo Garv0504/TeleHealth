432b3f3a8730086a115024d844f733321bc0ae00
Garv0504 <garv51104@gmail.com>
2025-05-09 12:07:25 +0530

    Editing Configuration

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import VideoCard from './Components/VideoCard'
+import Home from './Pages/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <h1>TeleHeath Web App</h1>
+        {/* <h1>TeleHeath Web App</h1> */}
+        <Home/>
+        <VideoCard/>
       </div>
     </>
   )
