4e7f6edd1086c6fd425538ee0f83e91ec79d14d9
Garv0504 <garv51104@gmail.com>
2025-05-09 16:48:07 +0530

    Routing Configuration

--- Backend/server.js
+++ Backend/server.js
@@ -25,7 +25,7 @@ app.use(express.json());
 // app.use(cookieParser());
 
 // // Enable CORS
-// app.use(cors());
+app.use(cors());
 
 // // Set security headers
 // app.use(helmet());
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,24 +5,15 @@ import Home from './Pages/Home'
 import Login from './Pages/login'
 import Register from './Pages/Register'
 import Footer from './Components/Footer'
+import { Routes, Route } from 'react-router-dom'
 function App() {
 
   return (
-    <>
-      <div>
-        {/* <h1>TeleHeath Web App</h1> */}
-
-        {/* <Home/> */}
-        {/* <Register/> */}
-        <Footer/>
-        {/* <Login/> */}
-        {/* <VideoCard/> */}
-
-        <Navbar/>
-        <Home/>
-        <VideoCard/>
-      </div>
-    </>
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} /> 
+          <Route path="/register" element={<Register />} />
+      </Routes>
   )
 }
 
