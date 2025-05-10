a61c74ba4940048967ce540d844794fe3e0623c2
devk007 <devkumardk037@gmail.com>
2025-05-10 12:09:15 +0530

    Dashboard Work

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,14 +6,16 @@ import Login from './Pages/login'
 import Register from './Pages/Register'
 import Footer from './Components/Footer'
 import { Routes, Route } from 'react-router-dom'
+import Specialties from './Components/Specialities'
 function App() {
 
   return (
-      <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} /> 
-          <Route path="/register" element={<Register />} />
-      </Routes>
+      // <Routes>
+      //     <Route path="/" element={<Home />} />
+      //     <Route path="/login" element={<Login />} /> 
+      //     <Route path="/register" element={<Register />} />
+      // </Routes>
+      <Specialties/>
   )
 }
 
