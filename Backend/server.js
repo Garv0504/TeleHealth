44807bd649552dedb4eecf75ed56fe7b09aef165
yaryan1303 <aryany1303@gmail.com>
2025-05-11 13:07:42 +0530

    after payment

--- Backend/server.js
+++ Backend/server.js
@@ -18,6 +18,7 @@ const authRoutes = require("./routes/authRoutes");
 const availabilityRoutes = require("./routes/availabilityRoutes");
 const appointmentRoutes = require("./routes/appointmentRoutes");
 const notificationRoutes = require("./routes/notificationRoutes");
+const paymentRoutes = require("./routes/payments");
 
 const app = express();
 
@@ -42,6 +43,7 @@ if (process.env.NODE_ENV === "development") {
 app.use("/api/auth", authRoutes);
 app.use("/api/availability", availabilityRoutes);
 app.use("/api/appointments", appointmentRoutes);
+app.use("/api/payment", paymentRoutes);
 
 // app.use("/", notificationRoutes);
 // Error handler middleware
